4e9776cd327c5fcc1e33021d28b8931a717b6030
aniket1020 <aniket.1020@gmail.com>
2020-08-01 15:13:08 +0530

    Used MySQL driver

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -2,6 +2,19 @@ var express = require('express');
 var mysql = require('mysql');
 var todoRouter = express.Router();
 
+var db = mysql.createConnection({
+	host:'localhost',
+	user:'admin',
+	password:'password',
+	database:'sample_project',
+	insecureAuth : true
+})
+
+db.connect((err)	=>	{
+	if(err)	throw err;
+	console.log("Connection successful");
+});
+
 todoRouter.route('/')
 .get((req,res,next)	=>	{
 	res.statusCode	=	200;
