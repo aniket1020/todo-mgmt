ef78c66272c191dd9896af577b16b1b8bcc515e0
aniket1020 <aniket.1020@gmail.com>
2020-08-01 14:15:37 +0530

    Initial setup

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var todoRouter = require('./routes/todoRouter');
 
 var app = express();
 
@@ -20,8 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Add routes here
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/todo',todoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
