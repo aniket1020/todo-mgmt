79fd1e098168337bad78ec40bbe147d9175214de
aniket1020 <aniket.1020@gmail.com>
2020-08-27 21:30:57 +0530

    Added basic API for GET, POST, DELETE

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -24,25 +24,32 @@ db.connect((err) => {
 
 //http://localhost:3000/todo
 todoRouter.route('/')
-	.get((req, res, next) => {
-		db.query('select * from '+req.user_id+';', (err,result) => {
+	.get((req, res) => {
+		db.query('select * from user_'+req.query.userid+';', (err,results) => {
 			if(err) throw err;
 			console.log("Fetching data from todo_db");
-			res.statusCode	=	200;
-			res.setHeader('Content-Type','application/json');
-			res.json(result);
+			res.status	=	200;
+			res.json(results);
 		})
-
 	})
-	.post((req, res, next) => {
-		db.query("insert into user_id values ();", (err) => {
+	.post((req, res) => {
+		console.log(req.body.userid);
+		db.query("insert into user_"+req.body.userid+" (title, body, time_rem) values ('"+req.body.title+"','"+req.body.body+"','"+req.body.time_rem+"');", (err,results) => {
 			if(err) throw err;
 			else {
-				res.end("Inserted Successfully");
+				res.status(200).send({err:false,data:results,message:'Inserted into the table successfully'});
 				console.log("Inserted into the table successfully");
 			}
 		})
 	})
-
+	.delete((req,res)	=>	{
+		db.query("delete from user_"+req.body.userid+" where assign_id	=	"+req.body.assign_id, (err) => {
+			if(err) throw err;
+			else {
+				res.status(200).send({err:false,data:results,message:'Delete from the table successfully'});
+				console.log("Delete from the table successfully");
+			}
+		})
+	})
 
 module.exports = todoRouter;
